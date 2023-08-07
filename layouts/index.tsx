2ceeb4c6b0d8d355f41670d89582868a8c26dc63
Akshat Mittal <akshatmittal2506@gmail.com>
2023-08-08 00:34:24 +0530

    feat: seo favicon setup and removed tests

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -6,8 +6,8 @@ const Layout: React.FC<any> = ({ children }) => {
 	return (
 		<>
 			<Seo
-				title="NextJS Boilerplate"
-				description="NextJS Boilerplate with TypeScript, ESLint, Prettier, Jest, React Testing Library, SASS, and more."
+				title="Akshat Mittal - Full Stack Developer"
+				description="Akshat Mittal - Full Stack Developer from India designing and building the web, applications and everything in between."
 				image="/images/og-image.png"
 				icons={["icon", "shortcut icon", "apple-touch-icon"].map(
 					(item) => {
@@ -22,16 +22,16 @@ const Layout: React.FC<any> = ({ children }) => {
 					card: "summary_large_image",
 					site: "@akshatmittal61",
 					author: "@akshatmittal61",
-					title: "NextJS Boilerplate",
+					title: "Akshat Mittal - Full Stack Developer",
 					description:
-						"NextJS Boilerplate with TypeScript, ESLint, Prettier, Jest, React Testing Library, SASS, and more.",
+						"Akshat Mittal - Full Stack Developer from India designing and building the web, applications and everything in between.",
 					image: "/images/og-image.png",
 					url: frontendBaseUrl,
 				}}
 				og={{
-					title: "NextJS Boilerplate",
+					title: "Akshat Mittal - Full Stack Developer",
 					description:
-						"NextJS Boilerplate with TypeScript, ESLint, Prettier, Jest, React Testing Library, SASS, and more.",
+						"Akshat Mittal - Full Stack Developer from India designing and building the web, applications and everything in between.",
 					images: [
 						{
 							url: "/images/og-image.png",
@@ -39,13 +39,14 @@ const Layout: React.FC<any> = ({ children }) => {
 							type: "image/png",
 							width: 1200,
 							height: 630,
-							alt: "NextJS Boilerplate",
+							alt: "Akshat Mittal - Full Stack Developer",
 						},
 					],
 					url: frontendBaseUrl,
 					type: "website",
-					siteName: "NextJS Boilerplate",
+					siteName: "Akshat Mittal - Full Stack Developer",
 				}}
+				themeColor="#011627"
 			/>
 			{children}
 		</>
