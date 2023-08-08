deb946836ba2f323a37127b696397d8f71392674
Akshat Mittal <akshatmittal2506@gmail.com>
2023-08-08 16:20:12 +0530

    feat: created logo overlay

---
+++ layouts/Overlay/index.tsx
@@ -0,0 +1,19 @@
+import { stylesConfig } from "@/utils/functions";
+import styles from "./styles.module.scss";
+import React from "react";
+
+const classes = stylesConfig(styles, "overlay");
+
+const Overlay: React.FC = () => {
+	return (
+		<main id="overlay" className={classes("")}>
+			<div className={classes("-logo")}>
+				<span className={classes("-logo-left")}></span>
+				<span className={classes("-logo-right")}></span>
+				<span className={classes("-logo-center")}></span>
+			</div>
+		</main>
+	);
+};
+
+export default Overlay;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import GlobalContext from "@/context/GlobalContext";
 import useContextData from "@/context/useContext";
 import Layout from "@/layouts";
+import Overlay from "@/layouts/Overlay";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import useLayoutEffect from "@/hooks/useIsomorphicLayoutEffect";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const context = useContextData();
+	const [showOverlay, setShowOverlay] = useState(true);
+	useLayoutEffect(() => {
+		const timer = setTimeout(() => {
+			setShowOverlay(false);
+		}, 3750);
+		return () => clearTimeout(timer);
+	}, []);
 	return (
 		<GlobalContext.Provider value={context}>
+			{showOverlay ? <Overlay /> : null}
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
