21ff2d2e6d82c7cc26a5baeb0dc88253074fe556
Akshat Mittal <akshatmittal2506@gmail.com>
2023-08-11 19:39:51 +0530

    ci: modified site seo

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -11,13 +11,13 @@ const Layout: React.FC<any> = ({ children }) => {
 		<>
 			<Seo
 				title="Akshat Mittal - Full Stack Developer"
-				description="Akshat Mittal - Full Stack Developer from India designing and building the web, applications and everything in between."
-				image="/images/og-image.png"
+				description="Akshat Mittal - Full Stack Developer, designing and building the web applications and everything in between."
+				image={`${frontendBaseUrl}/banner.png`}
 				icons={["icon", "shortcut icon", "apple-touch-icon"].map(
 					(item) => {
 						return {
 							rel: item,
-							href: "/favicon.ico",
+							href: `${frontendBaseUrl}/favicon.ico`,
 							type: "icon/ico",
 						};
 					}
@@ -28,18 +28,42 @@ const Layout: React.FC<any> = ({ children }) => {
 					author: "@akshatmittal61",
 					title: "Akshat Mittal - Full Stack Developer",
 					description:
-						"Akshat Mittal - Full Stack Developer from India designing and building the web, applications and everything in between.",
-					image: "/images/og-image.png",
+						"Akshat Mittal - Full Stack Developer, designing and building the web applications and everything in between.",
+					image: `${frontendBaseUrl}/banner.png`,
 					url: frontendBaseUrl,
 				}}
 				og={{
 					title: "Akshat Mittal - Full Stack Developer",
 					description:
-						"Akshat Mittal - Full Stack Developer from India designing and building the web, applications and everything in between.",
+						"Akshat Mittal - Full Stack Developer, designing and building the web applications and everything in between.",
 					images: [
 						{
-							url: "/images/og-image.png",
-							secureUrl: "/images/og-image.png",
+							url: `${frontendBaseUrl}/favicon-192.png`,
+							secureUrl: `${frontendBaseUrl}/favicon-192.png`,
+							type: "image/png",
+							width: 192,
+							height: 192,
+							alt: "Akshat Mittal - Full Stack Developer",
+						},
+						{
+							url: `${frontendBaseUrl}/favicon-512.png`,
+							secureUrl: `${frontendBaseUrl}/favicon-512.png`,
+							type: "image/png",
+							width: 512,
+							height: 512,
+							alt: "Akshat Mittal - Full Stack Developer",
+						},
+						{
+							url: `${frontendBaseUrl}/card.png`,
+							secureUrl: `${frontendBaseUrl}/card.png`,
+							type: "image/png",
+							width: 800,
+							height: 600,
+							alt: "Akshat Mittal - Full Stack Developer",
+						},
+						{
+							url: `${frontendBaseUrl}/banner.png`,
+							secureUrl: `${frontendBaseUrl}/banner.png`,
 							type: "image/png",
 							width: 1200,
 							height: 630,
@@ -51,6 +75,8 @@ const Layout: React.FC<any> = ({ children }) => {
 					siteName: "Akshat Mittal - Full Stack Developer",
 				}}
 				themeColor="#011627"
+				canonical={frontendBaseUrl}
+				siteName="Akshat Mittal - Full Stack Developer"
 			/>
 			<Navbar />
 			<SocialsBar />
