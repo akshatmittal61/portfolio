f46dd2eff947684791d3ec3f19617959b60cfb69
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-19 14:33:00 +0530

    style: setup for git attributes

--- context/useContext.ts
+++ context/useContext.ts
@@ -1,32 +1,11 @@
-import { IProject } from "@/types/project";
 import { useState } from "react";
+import { IProject } from "@/types/project";
+import projectsData from "@/data/projects";
 
 const useContextData = () => {
 	const [theme, setTheme] = useState<"light" | "dark">("light");
 
-	const projects: IProject[] = [
-		{
-			id: 1,
-			title: "City Tales",
-			description: "A social media app for sharing stories about cities",
-			github: "https://github.com/akshatmittal61/city-tales",
-			deployment: "https://citytales.org/",
-			image: "https://www.citytales.org/images/coming-soon.png",
-			logo: "https://www.citytales.org/images/primary-logo-1.png",
-			tags: ["nestjs", "typescript", "javascript", "sass"],
-		},
-		{
-			id: 2,
-			title: "Meraki IIIT Una",
-			description:
-				"The official website of Meraki: Annual Techfest of IIIT Una",
-			github: "https://github.com/meraki-iiitu/meraki-23",
-			deployment: "https://meraki-23.vercel.app/",
-			image: "https://meraki-23.vercel.app/favicon.png",
-			logo: "https://meraki-23.vercel.app/favicon.png",
-			tags: ["nextjs", "typescript", "javascript", "google-apis", "sass"],
-		},
-	];
+	const projects: IProject[] = projectsData;
 
 	return {
 		theme,
