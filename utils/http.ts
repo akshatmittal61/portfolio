51431af8357fec09e2999f4a02cc5b1df7d3982c
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-16 10:13:42 +0530

    feat: revamped typography

--- utils/http.ts
+++ utils/http.ts
@@ -5,13 +5,13 @@ export const http = axios.create({
 	baseURL: backendBaseUrl,
 });
 
-// ----------------------------------------------------------------
-//                     Default Axios with Token
-// ----------------------------------------------------------------
 http.interceptors.request.use(
 	async function (Config: any) {
 		const config = Config;
-		const token = localStorage.getItem("token");
+		let token = null;
+		if (typeof window !== "undefined" && localStorage) {
+			token = localStorage.getItem("token");
+		}
 		try {
 			if (token) {
 				config.headers["x-auth-token"] = `${token}`;
