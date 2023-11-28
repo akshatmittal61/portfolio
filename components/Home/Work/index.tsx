5fe1b91bdef2e1d62e872471df541886825c1b04
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-28 09:05:34 +0530

    feat: added work experience

--- components/Home/Work/index.tsx
+++ components/Home/Work/index.tsx
@@ -10,73 +10,90 @@ import moment from "moment";
 const classes = stylesConfig(styles, "home-work");
 
 const HomeWork: React.FC = () => {
-	const [active, setActive] = useState(-1);
+	const [active, setActive] = useState(workExperience[0].id);
 
 	return (
 		<div className={classes("")}>
 			<Typography size="head-3" weight="medium" as="h1">
 				Work History
 			</Typography>
-			{workExperience.map((work) => (
-				<div key={`work-${work.id}`} className={classes("-work")}>
-					<div className={classes("-work-head")}>
-						<a
-							href={work.company.link}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<Image
-								src={work.company.logo}
-								alt={work.company.name}
-								width={48}
-								height={48}
-								className={classes("-work-logo")}
-							/>
-						</a>
-						<div className={classes("-work-details")}>
-							<Typography
-								size="head-3"
-								weight="medium"
-								as="h3"
-								className={classes("-work-position")}
+			{workExperience.map((work, index) => (
+				<>
+					<div
+						key={`work-${work.id}`}
+						className={classes("-work", {
+							"-work--active": active === work.id,
+						})}
+					>
+						<div className={classes("-work-head")}>
+							<a
+								href={work.company.link}
+								target="_blank"
+								rel="noreferrer"
 							>
-								{work.position}
-							</Typography>
-							<div className={classes("-work-meta")}>
+								<Image
+									src={work.company.logo}
+									alt={work.company.name}
+									width={48}
+									height={48}
+									className={classes("-work-logo")}
+								/>
+							</a>
+							<div className={classes("-work-details")}>
 								<Typography
-									size="md"
+									size="head-3"
+									weight="regular"
 									as="h3"
-									className={classes("-work-company")}
+									className={classes("-work-position")}
 								>
-									{work.company.name}
+									{work.position}
 								</Typography>
-								<Calendar size={20} />
-								{moment(work.startDate).format("MMM YYYY")}
-								{" - "}
-								{work.endDate
-									? moment(work.endDate).format("MMM YYYY")
-									: "Present"}
+								<div className={classes("-work-meta")}>
+									<a
+										href={work.company.link}
+										target="_blank"
+										rel="noreferrer"
+										className={classes("-work-company")}
+									>
+										{work.company.name}
+									</a>
+									<Calendar size={20} />
+									{moment(work.startDate).format("MMM YYYY")}
+									{" - "}
+									{(() => {
+										if (work.endDate) {
+											return moment(work.endDate).format(
+												"MMM YYYY"
+											);
+										} else {
+											return "Present";
+										}
+									})()}
+								</div>
 							</div>
+							<button
+								className={classes("-work-toggle")}
+								onClick={() =>
+									setActive(active === work.id ? -1 : work.id)
+								}
+							>
+								<ChevronDown />
+							</button>
 						</div>
-						<button
-							className={classes("-work-toggle")}
-							onClick={() =>
-								setActive(active === work.id ? -1 : work.id)
-							}
-						>
-							<ChevronDown />
-						</button>
+						<div className={classes("-work-body")}>
+							<Typography
+								size="md"
+								as="p"
+								className={classes("-work-description")}
+							>
+								{work.description}
+							</Typography>
+						</div>
 					</div>
-					<div className={classes("-work-body")}>
-						<Typography
-							size="s"
-							as="p"
-							className={classes("-work-description")}
-						>
-							{work.description}
-						</Typography>
-					</div>
-				</div>
+					{index !== workExperience.length - 1 ? (
+						<hr className={classes("-divider")} />
+					) : null}
+				</>
 			))}
 		</div>
 	);
--- data/experience.tsx
+++ data/experience.tsx
@@ -2,21 +2,47 @@ import { IWork } from "@/types/work";
 
 const workExperience: IWork[] = [
 	{
-		id: 1,
+		id: 3,
 		company: {
 			name: "AthenaSquare",
-			logo: "https://athenasquare.org/mstile-144x144.png",
+			logo: "/images/work/athenasquare.png",
 			link: "https://athenasquare.org",
 		},
 		position: "SDE Intern",
-		description: `
-• Developed Screening Dashboard & real-time Application Tracking System for hiring automation
+		description: `• Developed Screening Dashboard & real-time Application Tracking System for hiring automation
 • Built Auth workflow using Google OAuth and candidate assessment workflow with proctoring
 • Migrated entire product from ReactJS project to NextJS for SEO optimization by 55%
-• Tech Stack: ReactJS, NextJS, TypeScript, Redux, SASS, Jest
-        `,
+• Tech Stack: ReactJS, NextJS, TypeScript, Redux, SASS, Jest`,
 		startDate: "2022-10-24T00:00:00.000Z",
 		tags: ["reactjs", "nextjs", "typescript", "jest", "sass"],
+	},
+	{
+		id: 2,
+		company: {
+			name: "StakeEasy",
+			logo: "/images/work/stakeeasy.png",
+			link: "https://www.stakeeasy.finance",
+		},
+		position: "Frontend Developer Intern",
+		description: `• Worked with frontend team on an interface for liquid staking protocol, blockchain platform
+• Used ReactJS for responsive user interface development`,
+		startDate: "2022-05-23T00:00:00.000Z",
+		endDate: "2022-07-18T00:00:00.000Z",
+		tags: ["reactjs", "sass", "typescript"],
+	},
+	{
+		id: 1,
+		company: {
+			name: "Techsnap",
+			logo: "/images/work/tehsnap.png",
+			link: "https://www.techsnap.in/",
+		},
+		position: "Frontend Developer Intern",
+		description: `• Built Figma prototype with interactive and engaging user design
+• Implemented and delivered multiple responsive pages with improved SEO optimization`,
+		startDate: "2021-01-01T00:00:00.000Z",
+		endDate: "2021-02-01T00:00:00.000Z",
+		tags: ["figma", "html", "css", "javascript"],
 	},
 ];
 
