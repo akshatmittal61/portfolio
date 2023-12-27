2986e3d7887aac1285d0624fd03caa4810b0e19d
Akshat Mittal <akshatmittal2506@gmail.com>
2023-12-27 18:44:39 +0530

    refactor: cleanup of removable utilities

--- utils/http.ts
+++ utils/http.ts
@@ -1,27 +1,8 @@
 import { backendBaseUrl } from "@/constants/variables";
 import axios from "axios";
 
-export const http = axios.create({
+const http = axios.create({
 	baseURL: backendBaseUrl,
 });
 
-http.interceptors.request.use(
-	async function (Config: any) {
-		const config = Config;
-		let token = null;
-		if (typeof window !== "undefined" && localStorage) {
-			token = localStorage.getItem("token");
-		}
-		try {
-			if (token) {
-				config.headers["x-auth-token"] = `${token}`;
-			}
-			return config;
-		} catch (err) {
-			return config;
-		}
-	},
-	function (error: Error) {
-		return Promise.reject(error);
-	}
-);
+export default http;
