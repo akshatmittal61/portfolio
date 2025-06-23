773791ff980a00ecd349a69456fdc29613155a17
Akshat Mittal <akshatmittal2506@gmail.com>
2025-06-24 04:37:42 +0530

    config: added sass

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+	/* config options here */
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [{ protocol: "https", hostname: "**" }],
+		dangerouslyAllowSVG: true,
+		domains: ["localhost"],
+	},
+	sassOptions: {
+		includePaths: [path.join(process.cwd(), "styles")],
+		quiteDeps: true,
+		silenceDeprecations: [
+			"legacy-js-api",
+			"import",
+			"color-functions",
+			"global-builtin",
+			"mixed-decls",
+		],
+	},
 };
 
 export default nextConfig;
