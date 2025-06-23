b81fd59fe3a70e80725e09ffab33a4443e4cd6d9
Akshat Mittal <akshatmittal2506@gmail.com>
2025-06-24 05:06:58 +0530

    feat: added hero section content

--- components/Home/Hero/index.tsx
+++ components/Home/Hero/index.tsx
@@ -2,10 +2,19 @@ import { stylesConfig } from "@/utils";
 import React from "react";
 import styles from "./styles.module.scss";
 
-const classes = stylesConfig(styles);
+const classes = stylesConfig(styles, "home-hero");
 
 type HomeHeroProps = {};
 
 export const HomeHero: React.FC<HomeHeroProps> = () => {
-	return <section id="" className={classes("")}></section>;
+	return (
+		<section id="" className={classes("")}>
+			<h1 className={classes("-name")}>Akshat Mittal</h1>
+			<p className={classes("-about")}>
+				Built for startups. Backed by experience. Powered by code.
+				<br />
+				Turning your bold ideas into fast, scalable web products.
+			</p>
+		</section>
+	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import styles from "@/styles/pages/Home.module.scss";
-// import { stylesConfig } from "@/utils";
+import styles from "@/styles/pages/Home.module.scss";
+import { stylesConfig } from "@/utils";
 import { HomeHero } from "@/components";
 
-// const classes = stylesConfig(styles);
+const classes = stylesConfig(styles, "home");
 
 const HomePage: React.FC = () => {
 	return (
-		<main className="w-full h-screen flex justify-center items-center">
+		<main className={classes("")}>
 			<HomeHero />
 		</main>
 	);
